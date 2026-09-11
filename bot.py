7986c32f033e7ba0f8ae86e8fa1470b97c1acfb0
s88386243-dotcom <s88386243@gmail.com>
2026-09-11 13:21:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,16 @@
 import os
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+@app.route('/')
+def home(): return "Bot is Alive!"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_flask).start()
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters, ContextTypes
 import yt_dlp
